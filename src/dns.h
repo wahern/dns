f0fe8c0cfe3a9d738e8783d3d055b687785c39be
William Ahern <william@william-aherns-macbook.local>
2009-03-16 19:13:09 -0400

    don't set _POSIX_C_SOURCE in dns.h; probably causes more headaches than not

--- src/dns.h
+++ src/dns.h
@@ -39,10 +39,6 @@
 
 #include <netinet/in.h>			/* struct in_addr struct in6_addr */
 
-#ifndef _POSIX_C_SOURCE
-#define _POSIX_C_SOURCE	200112L
-#endif
-
 #include <netdb.h>			/* struct addrinfo */
 #endif
 
