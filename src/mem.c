47a8db6bbde5c1c396a8d42f3c7db9053076f9b5
William Ahern <william@william-aherns-macbook.local>
2009-11-01 00:22:24 -0400

    some tweaks to mem.c heap code

--- src/mem.c
+++ src/mem.c
@@ -1,3 +1,29 @@
+/* ==========================================================================
+ * mem.c - Tiny heap.
+ * --------------------------------------------------------------------------
+ * Copyright (c) 2009  William Ahern
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so, subject to the
+ * following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+ * NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * ==========================================================================
+ */
+
 #include <limits.h>	/* CHAR_BIT */
 
 #include <string.h>	/* strlen(3) */
@@ -6,18 +32,19 @@
 
 #include <sys/param.h>	/* howmany setbit clrbit isset */
 
-#define PAGESIZE  4096
-#define SLOTSIZE  16
+
+#define PAGESIZE  4096U
+#define SLOTSIZE  16U
 #define SLOTCOUNT (PAGESIZE/SLOTSIZE)
 
-#define FIRSTFIT 0
-#define BESTFIT  1
-#define HOWFIT   BESTFIT
+#define BESTFIT  (1U<<0U)
 
 
 struct page {
 	unsigned char index[howmany(SLOTCOUNT, CHAR_BIT)];
 	unsigned char ptr[howmany(SLOTCOUNT, CHAR_BIT)];
+
+	unsigned long flags;
 
 	unsigned char data[PAGESIZE];
 }; /* struct page */
@@ -71,8 +98,7 @@ void *get(struct page *page, size_t size) {
 	if (!slot.count)
 		{ errno = ENOMEM; return 0; }
 
-#if HOWFIT == BESTFIT
-	if (slot.count != count) {
+	if ((page->flags & BESTFIT) && slot.count != count) {
 		struct slot tmp;
 
 		do {
@@ -84,7 +110,6 @@ void *get(struct page *page, size_t size) {
 				slot = tmp;
 		} while (tmp.count);
 	}
-#endif
 
 	p = &page->data[slot.base * SLOTSIZE];
 
@@ -124,6 +149,7 @@ char *sdup(struct page *page, const char *src) {
 
 #if 1
 
+#include <stddef.h>	/* offsetof */
 #include <stdlib.h>	/* EXIT_FAILURE */
 #include <stdio.h>	/* fputc(3) fputs(3) fprintf(3) */
 
@@ -199,38 +225,51 @@ int main(int argc, char *argv[]) {
 		  191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251 };
 	struct page page;
 	char word[256], *str;
-	int opt, frag = 0;
+	int opt, fflag = 0;
 	unsigned i;
 
 	setlocale(LC_ALL, "C");
 
-	while (-1 != (opt = getopt(argc, argv, "fh"))) {
+	memset(&page, 0, sizeof page);
+
+	while (-1 != (opt = getopt(argc, argv, "bfzh"))) {
 		switch (opt) {
+                case 'b':
+                        page.flags |= BESTFIT;
+                        break;
 		case 'f':
-			frag = 1;
+			fflag = 1;
+
+			for (i = 0; i < sizeof primes / sizeof primes[0] && i < SLOTCOUNT; i++)
+				setbit(page.index, primes[i]);
+
 			break;
+		case 'z':
+                	printf("(struct page)  : %zu\n", sizeof page);
+                	printf("offsetof(data) : %zu\n", offsetof(struct page, data));
+
+                	return 0;
 		default:
 			fprintf(stderr,
-				"%s [-fh]\n"
+				"%s [-bfzh]\n"
+				"  -b  Best fit\n"
 				"  -f  Fragment memory\n"
+				"  -z  Print data structure sizes\n"
 				"  -h  Print usage\n\n"
 				"Report bugs to <william@25thandClement.com>\n", argv[0]);
 			return (opt == 'h')? 0 : EXIT_FAILURE;
 		} /* switch() */
 	}
 
-	memset(&page, 0, sizeof page);
-
-	for (i = 0; frag && i < sizeof primes / sizeof primes[0] && i < SLOTCOUNT; i++)
-        	setbit(page.index, primes[i]);
-
 	while (EOF != scanf("%256s", word)) {
 		if (!(str = sdup(&page, word)))
 			perror("sdup");
 	}
 
-	for (i = 0; frag && i < sizeof primes / sizeof primes[0] && i < SLOTCOUNT; i++)
-		clrbit(page.index, primes[i]);
+	if (fflag) {
+		for (i = 0; i < sizeof primes / sizeof primes[0] && i < SLOTCOUNT; i++)
+			clrbit(page.index, primes[i]);
+	}
 
 	printpage(&page, stdout);
 
