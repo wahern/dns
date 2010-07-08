03c22406daa2959334745f430fa5ed4e6f770ed5
William Ahern <william@william-aherns-macbook.local>
2010-07-08 16:32:26 -0700

    bump version numbers

--- src/dns.h
+++ src/dns.h
@@ -64,9 +64,9 @@
 
 #define DNS_VENDOR "william@25thandClement.com"
 
-#define DNS_V_REL  0x20100623
-#define DNS_V_ABI  0x20100623
-#define DNS_V_API  0x20100503
+#define DNS_V_REL  0x20100709
+#define DNS_V_ABI  0x20100709
+#define DNS_V_API  0x20100709
 
 
 const char *dns_vendor(void);
